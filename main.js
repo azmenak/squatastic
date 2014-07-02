2e79d4a5c6f58a0c90ef2503d3d36cb1d3cb1e19
Adam Zmenak <adam@dinteractive.ca>
2014-07-02 13:46:29 -0400

    record a timeseries

--- main.js
+++ main.js
@@ -4,64 +4,67 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   var Counter = React.createClass({
     mixins: [ReactFireMixin],
-    
+
     getInitialState: function() {
       return {
         squats: {
           count: 0,
           timestamp: null
-        }
+        },
+        timeseries: []
       };
-    },          
+    },
 
     componentWillMount: function() {
-      this.bindAsObject(new Firebase("https://sq-count.firebaseio.com/squats"), "squats");
+      this.bindAsObject(
+        new Firebase("https://sq-count.firebaseio.com/squats"),
+                     "squats"
+      );
+      this.bindAsArray(
+        new Firebase("https://sq-count.firebaseio.com/timeseries"),
+                     "timeseries"
+      );
     },
 
     incrementSquat: function(event) {
-      this.firebaseRefs['squats'].transaction(function(currentObj) {
-        if (((new Date()).valueOf() - currentObj.timestamp) > 10000) {
-          currentCount = currentObj.count;
+      var time = Date.now()
+      var _this = this
+      _this.firebaseRefs['squats'].transaction(function(currentObj) {
+        if ((time - currentObj.timestamp) > 10000) {
+          _this.firebaseRefs['timeseries'].push(time);
+          var currentCount = currentObj.count;
           return {
             count: currentCount + 1,
-            timestamp: (new Date()).valueOf()
+            timestamp: time
           };
         }
       });
     },
 
-    decrementSquat: function(event) {
-      this.firebaseRefs['squats'].transaction(function(currentObj) {
-        currentCount = currentObj.count;
-        return {
-          count: currentCount - 1,
-          timestamp: (new Date()).valueOf()
-        };
-      });
-    },
-
-
     render: function() {
       return div({
         id: 'root'
-      }, 
+      },
         div({
           className: 'squats well'
         },
-          R.h2(null, 'Squats',
-            R.span({className: 'realtime-sqaut-count label label-default'}, this.state.squats.count)
+          R.h2({
+            style: {
+              'marginRight': '5px'
+            }
+          }, 'Squats: ',
+            R.span({className: 'realtime-sqaut-count label label-default'},
+                   this.state.squats.count)
           ),
           R.button({
             className: 'super-button btn btn-default',
             ref: 'incrementer',
-            onClick: this.incrementSquat
-          }, '+1 Squat'),
-          R.button({
-            className: 'mega-button btn btn-default',
-            ref: 'decrementter',
-            onClick: this.decrementSquat
-          }, '-1 Squat')
-        )  
+            onClick: this.incrementSquat,
+            style: {
+              'marginRight': '5px'
+            }
+          }, '+1 Squat')
+        )
       );
     }
   });
